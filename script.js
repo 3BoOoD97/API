526ad13e9bc3e9fc5513540e674439786db81476
Abdalrahman Dabor <ad222uk@student.lnu.se>
2022-04-04 21:53:55 +0200

    Improving code readability

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
+const api = "https://www.breakingbadapi.com/api/characters"
+
 async function get() {
-    const response = await fetch("https://www.breakingbadapi.com/api/characters")
+    const response = await fetch(api)
     const data = await response.json()
-    console.log(data)
 
-    data.map(function (actor) {
-        console.log(actor.name)
-    })
+    printData(data)
 
-    document.querySelector("#content h1").innerHTML = data[0].name
-    document.querySelector("#content h5").innerHTML = data[0].birthday
-    document.querySelector("#content img").src = data[0].img
-    document.querySelector("#actor").innerHTML =
-        `<select>
 
-        ${data.map(actor => `<option> ${actor.name} </option>`)
 
-        }
-  
-    </select>
-    `
 }
+
+function printData(data) {
+    const header = document.querySelector("#header")
+    const contnet = document.querySelector("#content")
+    header.innerHTML += `
+    <select class="form-control">
+    <option> Please Select </option>
+    ${data.map(actor => `<option>${actor.name}</option>`)}
+    </select> `
+
+}
+
+
 
 get()
